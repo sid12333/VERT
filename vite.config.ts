d9f3b21db37da94690cb09f51336d634eeb9bb0e
not-nullptr <62841684+not-nullptr@users.noreply.github.com>
2025-04-15 15:11:13 +0100

    feat: oops wait no THIS is icns support

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import svg from "@poppanator/sveltekit-svg";
+import wasm from "vite-plugin-wasm";
 
 export default defineConfig({
 	plugins: [
@@ -44,6 +45,7 @@ export default defineConfig({
 				},
 			],
 		}),
+		wasm(),
 	],
 	optimizeDeps: {
 		exclude: [
