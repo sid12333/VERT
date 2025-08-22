b4a650c3c29b9813b60567477cf1e3561388a07d
Denzel <denzel.mitski@gmail.com>
2025-08-22 04:13:04 +0200

    fix: empty file name fallback

--- src/lib/store/index.svelte.ts
+++ src/lib/store/index.svelte.ts
@@ -239,7 +239,7 @@ class Files {
 		const url = URL.createObjectURL(blob);
 
 		const settings = JSON.parse(localStorage.getItem("settings") ?? "{}");
-		const filenameFormat = settings.filenameFormat ?? "VERT_%name%";
+		const filenameFormat = settings.filenameFormat || "VERT_%name%";
 
 		const format = (name: string) => {
 			const date = new Date().toISOString();
