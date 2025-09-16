3686c881aee898fce10b0adfa2a086e78983b939
Maya <jovannmc@femboyfurry.net>
2025-09-16 19:46:31 +0300

    fix: add to available locales

--- src/lib/store/index.svelte.ts
+++ src/lib/store/index.svelte.ts
@@ -324,6 +324,7 @@ export const locale = writable(getLocale());
 export const availableLocales = {
 	en: "English",
 	es: "Español",
+	hr: "Hrvatski",
 };
 
 export function updateLocale(newLocale: string) {
